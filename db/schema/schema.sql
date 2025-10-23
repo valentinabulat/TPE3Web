ed2ca9c87065347e443cc027d28376f70e40d39d
Abril <juarezabrilvalentina@gmail.com>
2025-10-23 12:00:34 -0300

    cambios

--- db/schema/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE productos (
-    ID SERIAL PRIMARY KEY,
-    titulo VARCHAR(255) NOT NULL,
-    descripcion VARCHAR(255) NOT NULL,
-    cantidad INT NOT NULL, 
-    comprado BOOLEAN DEFAULT FALSE
-);
--- go.mod
+++
@@ -1,5 +0,0 @@
-module TPEWeb.com/TPEWeb
-
-go 1.24.6
-
-require github.com/lib/pq v1.10.9 // indirect
