b1ae465fa3da4068042396fa11cdb383ecea8054
valenbulat@gmail.com <valenbulat@gmail.com>
2025-11-13 10:52:02 -0300

    .

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"net/http"
 	"os"
-
+	"strconv"
+	
+	"github.com/valentinabulat/TPE3Web/pkg/views"
 	"github.com/valentinabulat/TPE3Web/pkg/handlers"
 
 	_ "github.com/lib/pq"
@@ -43,18 +45,81 @@ func main() {
 	//ctx := context.Background()
 
 	// crear instancia de api
-	api := handlers.NewAPI(queries)
+	//api := handlers.NewAPI(queries)
 
-	mux := http.NewServeMux()
-	fs := http.FileServer(http.Dir("./static"))
+	//mux := http.NewServeMux()
+	//fs := http.FileServer(http.Dir("./static"))
 
 	// configurar endpoints
-	mux.HandleFunc("/products", api.ProductsHandler)
-	mux.HandleFunc("/products/", api.ProductHandler)
-	mux.Handle("/", fs)
+	//mux.HandleFunc("/products", api.ProductsHandler)
+	//mux.HandleFunc("/products/", api.ProductHandler)
+	//mux.Handle("/", fs)
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		//Obtenga todos los registros de la base de datos usando el método List... de sqlc
+		productos, err := queries.ListProducts(r.Context())
+		if err != nil {
+			http.Error(w, "Error al obtener los productos", http.StatusInternalServerError)
+			return
+		}
+		
+		component := views.IndexPage(productos)
 
+		// Renderice el componente completo en el http.ResponseWriter.
+		err = component.Render(r.Context(),w)
+		if err != nil {
+			http.Error(w, "Error al renderizar la página", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	http.HandleFunc("POST /products", func(w http.ResponseWriter, r *http.Request) {
+		//Parsee los datos del formulario.
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "Error al parsear el formulario", http.StatusBadRequest)
+			return
+		}
+
+		//Obtenga los valores del formulario.
+		titulo := r.FormValue("titulo")
+		descripcion := r.FormValue("descripcion")
+		cantidadStr := r.FormValue("cantidad")
+		
+		// chequear valores vacios
+		if titulo == "" || descripcion == "" || cantidadStr == "" {
+			http.Error(w, "Todos los campos son obligatorios", http.StatusBadRequest)
+			return
+		}
+
+		// formatear cantidad a int
+		cantidad, err := strconv.Atoi(cantidadStr)
+		if err != nil {
+			http.Error(w, "Cantidad inválida", http.StatusBadRequest)
+			return
+		}
+
+		// chequear cantidad valida
+		if cantidad < 0 {
+			http.Error(w, "La cantidad no puede ser negativa", http.StatusBadRequest)
+			return
+		}
+
+		//Inserte un nuevo registro en la base de datos usando el método Create... de sqlc.
+		err := queries.CreateProduct(r.Context(), db.CreateProductParams{
+			Titulo:  titulo, 
+			Descripcion: descripcion,
+			Cantidad: int32(cantidad), // CHEQUEAR que los valores sean del tipo correcto
+		})
+		if err != nil {
+			http.Error(w, "Error al crear el producto", http.StatusInternalServerError)
+			return
+		}
+
+		// Redirija al usuario de vuelta a la página principal.
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+	
 	// iniciar servidor
 	log.Printf("Servidor escuchando en http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/valentinabulat/TPE3Web
 go 1.25.3
 
 require github.com/lib/pq v1.10.9
+
+require github.com/a-h/templ v0.3.960 // indirect
